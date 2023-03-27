770167076e7a3d5699aeb3fd1ca6a42e0d410996
sperky7933 <sperky7933@gmail.com>
2023-03-27 16:02:14 -0400

    added the nonterminals, need to make a seperate token class so i can iterate tokens easily

--- VSPL.java
+++ VSPL.java
@@ -8,7 +8,42 @@ import java.util.Scanner;
 
 public class VSPL{
 
-	public static List<String> read(String filename) throws Exception {
+    private static String program(String token){
+
+        if(token == "{"){
+            token
+            statement_list
+        }
+
+    }
+
+    private static String statement_list(String token){
+
+    }
+
+    private static String statement(String token){
+
+    }
+
+    private static String procedure_call(String token){
+
+    }
+
+    private static String parameters(String token){
+
+    }
+
+    private static String expression(String token){
+
+    }
+
+    private static String factor(String token){
+
+    }
+
+
+
+	private static List<String> read(String filename) throws Exception {
 		
         List<String> text = new ArrayList<String>(); 
     
@@ -43,6 +78,16 @@ public class VSPL{
         List<String> textList = new ArrayList<String>(); 
         textList = read(file);
         System.out.println("Grammar: \n\n"+textList);
+
+        for(int i = 0;i < textList.size(); i++){
+            String token = textList.get(i);
+
+            System.out.println("\n");
+            System.out.println("Current Token: "+token);
+
+
+
+        }
 
         
 
